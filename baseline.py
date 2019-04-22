e8274e456eb31bd9c24bce9ef640ba0348fcaf80
christian <christiancleber@gmail.com>
2019-04-22 17:01:00 -0400

    Ok

--- baseline.py
+++ baseline.py
@@ -50,9 +50,11 @@ def firstExecution():
     tokens = CountVectorizer(max_features=10000, lowercase=True, binary=True)
     X_train = tokens.fit_transform(X_train['comment_text'])
     X_test = tokens.transform(X_test['comment_text'])
-
+    # save the test set to be able to test the model later
     scipy.sparse.save_npz('baseline_balanced_test_X.npz', X_test)
     Y_test.to_csv('baseline_balanced_test_Y.csv', header=['id', 'toxic'])
+    # save the vocabulary
+    pickle.dump(tokens.vocabulary_, open('vocabulary.save', 'wb'))
 
     return X_train , X_test , Y_train['toxic'] , Y_test['toxic']
 
@@ -85,7 +87,7 @@ def firstExecution():
 #firstExecution()
 
 
-# Now begins the evaluation of the model regarding bias
+# Now starts the evaluation of the model regarding bias
 
 X_test = scipy.sparse.load_npz('baseline_balanced_test_X.npz')
 Y_test = pd.read_csv('baseline_balanced_test_Y.csv', sep = ',', usecols=['toxic'])
@@ -94,7 +96,71 @@ pred = loaded_model.predict_proba(X_test)[:, 1]
 auc = roc_auc_score(Y_test, pred)
 print('Test ROC AUC: %.3f' %auc)
 print(loaded_model.score(X_test, Y_test))
-print(sklearn.metrics.confusion_matrix(Y_test, pred>0.6))
+print(sklearn.metrics.confusion_matrix(Y_test, pred>0.5))
+
+print('\nUsing the debias dataset \n')
+
+debias_path = "/home/christian/GWU/Bias in AI/unintended-ml-bias-analysis-master/unintended_ml_bias/eval_datasets/bias_madlibs_77k.csv"
+debias_test = pd.read_csv(debias_path)
+debias_test['Label'] = debias_test['Label'].apply(lambda x: 1 if x == 'BAD' else 0)
+vocabulary = pickle.load(open('vocabulary.save', 'rb'))
+tokens = CountVectorizer(max_features=10000, lowercase=True, binary=True, vocabulary = vocabulary)
+X_test = tokens.fit_transform(debias_test['Text'])
+Y_test = debias_test['Label']
+pred = loaded_model.predict_proba(X_test)[:, 1]
+auc = roc_auc_score(Y_test, pred)
+print('Test ROC AUC: %.3f' %auc)
+print(loaded_model.score(X_test, Y_test))
+print(sklearn.metrics.confusion_matrix(Y_test, pred>0.5))
+
+print('\nUsing the fuzzy dataset \n')
+
+# This technique involves modifying a test set by "fuzzing" over a set of identity terms in order to evaluate a model
+# for bias. Given a test set and a set of terms, we replace all instances of each term in the test data with a
+# random other term from the set. The idea is that the specific term used for each example should not be the key
+# feature in determining the label for the example. For example, the sentence "I had a friend growing up" should
+# be considered non-toxic, and "All people must be wiped off the earth" should be considered toxic for all values
+# of x in the terms set.
+#
+# The code in src/Bias_fuzzed_test_set.ipynb reads the Wikipedia Toxicity dataset and builds an identity-term-focused
+# test set. It writes unmodified and fuzzed versions of that test set. One can then evaluate a model on both test sets
+# . Doing significantly worse on the fuzzed version may indicate a bias in the model. The datasets are
+# eval_datasets/toxicity_fuzzed_testset.csv and eval_datasets/toxicity_nonfuzzed_testset.csv. Each CSV consists of
+# 3 columns: ID unedr rev_id, the comment text under comment, and the True/False label under toxic.
+#
+# This is similar to the bias madlibs technique, but has the advantage of using more realistic data.
+# One can also use the model's performance on the original vs. fuzzed test set as a bias metric.
+
+print('First the fuzzed \n')
+debias_path = "/home/christian/GWU/Bias in AI/unintended-ml-bias-analysis-master/unintended_ml_bias/eval_datasets/toxicity_fuzzed_testset.csv"
+debias_test = pd.read_csv(debias_path)
+#debias_test['toxic'] = debias_test['toxic'].apply(lambda x: 1 if x == 'True' else 0)
+vocabulary = pickle.load(open('vocabulary.save', 'rb'))
+tokens = CountVectorizer(max_features=10000, lowercase=True, binary=True, vocabulary = vocabulary)
+X_test = tokens.fit_transform(debias_test['comment'])
+Y_test = debias_test['toxic']
+pred = loaded_model.predict_proba(X_test)[:, 1]
+auc = roc_auc_score(Y_test, pred)
+print('Test ROC AUC: %.3f' %auc)
+print(loaded_model.score(X_test, Y_test))
+print(sklearn.metrics.confusion_matrix(Y_test, pred>0.5))
+
+print('\n Now the original test set \n')
+debias_path = "/home/christian/GWU/Bias in AI/unintended-ml-bias-analysis-master/unintended_ml_bias/eval_datasets/toxicity_nonfuzzed_testset.csv"
+debias_test = pd.read_csv(debias_path)
+#debias_test['toxic'] = debias_test['toxic'].apply(lambda x: 1 if x == 'True' else 0)
+vocabulary = pickle.load(open('vocabulary.save', 'rb'))
+tokens = CountVectorizer(max_features=10000, lowercase=True, binary=True, vocabulary = vocabulary)
+X_test = tokens.fit_transform(debias_test['comment'])
+Y_test = debias_test['toxic']
+pred = loaded_model.predict_proba(X_test)[:, 1]
+auc = roc_auc_score(Y_test, pred)
+print('Test ROC AUC: %.3f' %auc)
+print(loaded_model.score(X_test, Y_test))
+print(sklearn.metrics.confusion_matrix(Y_test, pred>0.5))
+
+
+
 
 
 # Training with cross-validation
